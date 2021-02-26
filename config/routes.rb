520d179001dadf9b40249c3659e1fb57bf4021d6
ricardoeavd <ricardoeavd@chicagobooth.edu>
2021-02-26 15:39:15 +0000

    Trial 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,42 @@
 Rails.application.routes.draw do
 
+  # Routes for the User account:
+
+  # SIGN UP FORM
+  get("/user_sign_up", { :controller => "user_authentication", :action => "sign_up_form" })        
+  # CREATE RECORD
+  post("/insert_user", { :controller => "user_authentication", :action => "create"  })
+      
+  # EDIT PROFILE FORM        
+  get("/edit_user_profile", { :controller => "user_authentication", :action => "edit_profile_form" })       
+  # UPDATE RECORD
+  post("/modify_user", { :controller => "user_authentication", :action => "update" })
+  
+  # DELETE RECORD
+  get("/cancel_user_account", { :controller => "user_authentication", :action => "destroy" })
+
+  # ------------------------------
+
+  # SIGN IN FORM
+  get("/user_sign_in", { :controller => "user_authentication", :action => "sign_in_form" })
+  # AUTHENTICATE AND STORE COOKIE
+  post("/user_verify_credentials", { :controller => "user_authentication", :action => "create_cookie" })
+  
+  # SIGN OUT        
+  get("/user_sign_out", { :controller => "user_authentication", :action => "destroy_cookies" })
+             
+  #------------------------------
+
+# Homepage
+get("/user_diet_plan", {:controller => "users", :action => "index"})
+
+
+# User routes
+get("/", {:controller => "users", :action => "sign_in"})
+post("/verify_credentials", {:controller => "users", :action => "verify_credentials"})
+get("/user_sign_up", {:controller => "users", :action => "new_registration_form"})
+get("/user_sign_out", {:controller => "users", :action => "sign_out"})
+get("/insert_user_record", {:controller => "users", :action => "create" })
 
 
 end
